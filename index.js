d5088d3b9ad066652d056fecf7d2f80b9ff3372f
gfaruk02 <gfaruk02@gmail.com>
2023-11-05 13:00:06 +0600

    complete mongodb connetion and its successfully connected to MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,12 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.zjs4f1h.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.MY_DB_USER}:${process.env.My_BD_PASS}@cluster0.zjs4f1h.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
